0cbaaf776a9d5ddadc3d41f94cbd26fc54633fe5
shivamshankarpandey100 <shivamshankarpandey100@gmail.com>
2023-06-18 20:14:02 +0530

    18-06-2023

---
+++ 2d array/wavedisplay.java
@@ -0,0 +1,29 @@
+ import java.util.*;
+ public class Main{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int r=sc.nextInt();
+        int c=sc.nextInt();
+        int[][] arr=new int[r][c];
+
+        for(int i=0;i< arr.length;i++){
+            for(int j=0;j<arr[0].length;j++){
+                arr[i][j]=sc.nextInt();
+            }
+        }
+         for(int j=0;j<arr[0].length;j++){
+            if(j%2==0){
+                for(int i=0;i<arr.length;i++){
+                System.out.println(arr[i][j]);
+            }
+            
+            }
+            else{
+                for(int i=arr.length-1;i>=0;i--){
+                    System.out.println(arr[i][j]);
+                }
+            }
+         }
+
+        }
+ }
