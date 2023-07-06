d52c17a76c1aea99b7b529304553226e9dbfff86
shivamshankarpandey100 <shivamshankarpandey100@gmail.com>
2023-07-06 10:39:03 +0530

    06-07-2023

---
+++ 2d array/diagonaldisplay.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class Main{
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+         int [][]arr=new int[n][n];
+         for(int i=0;i<arr.length;i++){
+            for(int j=0;j<arr[0].length;j++){
+                int arr[i][j]=sc.nextint();
+            }
+         }
+        for(int g=0;g<arr[0].length;g++){
+            for(int i=0,j=g;j<arr[0].length;i++,j++){
+                System.out.println(arr[i][j]);
+            }
+        }
+
+
+
+    }
+}
