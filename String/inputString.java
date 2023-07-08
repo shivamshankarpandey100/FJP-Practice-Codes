353e4dee22957f453e8016f1caa1ec094cb7cfb7
shivamshankarpandey100 <shivamshankarpandey100@gmail.com>
2023-07-08 11:29:17 +0530

    08-07-2023

---
+++ String/inputString.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Main{
+    public static void main(String[][]args){
+        Scanner sc=new Scanner(System.in);
+        String s=sc.nextLine();
+        System.out.println(s);//if you write only next then interpreter stop on space 
+        //if you write nextLine interpreter red all words which is input with space
+        
+    }
+}
