aa88bb0d83a4194268761e735b10ca61f39497ba
shivamshankarpandey100 <shivamshankarpandey100@gmail.com>
2023-07-10 11:35:54 +0530

    10-07-20232

---
+++ Recursion/printdecresingincresing.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Main{
+    public static void main(String []args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+
+         PDI(n);
+    }
+    public static void PDI(int n){
+        if(n==0){
+            return;
+        }
+        System.out.println(n);
+        PDI(n-1);
+        System.out.println(n);
+    }
+}
