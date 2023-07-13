a065e4f8fba3f05dc85b306749c264243cbe970b
shivamshankarpandey100 <shivamshankarpandey100@gmail.com>
2023-07-13 11:15:04 +0530

    13-07-2023

--- Recursion/powerlogaritham.java
+++ Recursion/powerlogaritham.java
@@ -13,9 +13,9 @@ public class Main {
     }
 
     public static int power(int x, int n){
-        if(n==0{
+        if(n==0){
             return 1;
-        })
+        }
         int xpb2=power(x,n/2);
         int xpn=xpb2*xpb2;
         if(n%2==1){
