51f3d267ea7d5f025c9317e9fe2ad5096a2638e8
shivamshankarpandey100 <shivamshankarpandey100@gmail.com>
2023-07-16 09:57:22 +0530

    16-07-2023

---
+++ Recursion in array/displayArray.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        // write your code here
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int arr[][]=new int[n];
+        for(int i=0;i<arr.length;i++){
+            arr[i]=sc.nextInt();
+        }
+        
+        displayArr(arr,0);
+    }
+
+    public static void displayArr(int[] arr, int i){
+        System.out.println(arr[i]);
+        displayArr(arr,i+1);
+    }
+
+}
