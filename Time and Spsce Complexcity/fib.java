93a705099cb5cbe47d09f56e9cf76efd38d3a725
Shivam Shankar Pandety <shivamshankarpandey100@gmail.com>
2023-09-18 12:23:36 +0530

    fibonaki series is done

--- Time and Spsce Complexcity/fib.java
+++ Time and Spsce Complexcity/fib.java
@@ -1,3 +1,15 @@
+import java.util.*;
+public class Main{
+    pusblic static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int fn=fib(n);
+        System.out.println(fibn);
+    }
+}
+
+
+
 public static int fib(n){
     if(n==0 || n==1){
         return n;
