716b7591544d847adff85ee82b9f7d3eac519389
Shivam Shankar Pandety <shivamshankarpandey100@gmail.com>
2023-09-18 12:36:26 +0530

    error solve

--- Time and Spsce Complexcity/fib.java
+++ Time and Spsce Complexcity/fib.java
@@ -1,18 +1,18 @@
 import java.util.*;
 public class Main{
-    pusblic static void main(String[]args){
+    public static void main(String[]args){
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
         int fn=fib(n);
-        System.out.println(fibn);
+        System.out.println(fn);
     }
-    public static int fib(n){
+    public static int fib(int n){
     if(n==0 || n==1){
         return n;
     }
     int finnm1=fib(n-1);
     int finnm2=fib(n-2);
-    int fibn =fibnm1+fibnm2;
+    int fibn =finnm1+finnm2;
     return fibn;
 } 
 }
