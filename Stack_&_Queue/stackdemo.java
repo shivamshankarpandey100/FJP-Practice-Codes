1a4e11dad36d5001680cfc3954d514e6c18f250e
Shivam Shankar Pandety <shivamshankarpandey100@gmail.com>
2024-01-17 12:30:50 +0530

    Stack demo

---
+++ Stack_&_Queue/stackdemo.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Main{
+    public Ststic void main(String[] args){
+        Stack<Intiger> stack=new Stack<>();
+        stack.push(10);
+        stack.push(20);
+        stack.push(30);
+        stack.push(40);
+        Systeem.out.println(stack.peek());
+        stack.pop();
+
+        Systeem.out.println(stack.peek());
+    }
+}
