546d8855e90bddad84e56c43ae658ff4245bbd58
Shivam Shankar Pandety <shivamshankarpandey100@gmail.com>
2024-04-08 13:45:38 +0530

    :memo::zap: Tilling With 2 * 1 tiles :zap::memo:

---
+++ Dynamic_Programing/20.TilingWith2into1tiles.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Main219 {
+
+    public static void main(String[] args) throws Exception {
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int []dp = new int[n + 1];
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 2;
+
+        for(int i = 3; i <= n; i++){
+            dp[i] = dp[i - 1]  + dp[i - 2];
+        }
+
+        System.out.println(dp[n]);
+
+        sc.close();
+    }
+}
